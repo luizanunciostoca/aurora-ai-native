ce8ab76c0c2dd2d2de3465387221271c5b2027f6
Idebook Digital <luizanunciostoca@gmail.com>
2026-08-29 21:54:12 -0300

    build(W01-G): make context runtime schema type declaration-nameable

--- packages/schemas/src/context/internal.ts
+++ packages/schemas/src/context/internal.ts
@@ -1,7 +1,8 @@
 export type SafeParseResult<T> =
-  { readonly success: true; readonly data: T } | { readonly success: false; readonly error: Error };
+  | { readonly success: true; readonly data: T }
+  | { readonly success: false; readonly error: Error };
 
-interface RuntimeSchema<T> {
+export interface RuntimeSchema<T> {
   readonly parse: (value: unknown) => T;
   readonly safeParse: (value: unknown) => SafeParseResult<T>;
 }
