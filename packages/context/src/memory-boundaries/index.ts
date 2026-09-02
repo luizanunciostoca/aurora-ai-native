9c292b51759a589f5bb73837ae20f3092329dc4b
Idebook Digital <luizanunciostoca@gmail.com>
2026-09-02 02:31:12 -0300

    feat(W06-E): export memory boundary leaf

---
+++ packages/context/src/memory-boundaries/index.ts
@@ -0,0 +1,15 @@
+export { MEMORY_BOUNDARY_DESCRIPTORS, validateMemoryBoundaryCandidate } from './model.js';
+export type {
+  MemoryBoundaryCandidate,
+  MemoryBoundaryDescriptor,
+  MemoryBoundaryKind,
+  MemoryBoundaryValidationReason,
+  MemoryBoundaryValidationRequest,
+  MemoryBoundaryValidationResult,
+  MemoryConflictSemantics,
+  MemoryFreshnessSemantics,
+  MemoryReadProjectionOwner,
+  MemoryRetentionMode,
+  MemorySourceOwner,
+} from './types.js';
+export { MEMORY_BOUNDARY_KINDS, MEMORY_SOURCE_OWNERS } from './types.js';
