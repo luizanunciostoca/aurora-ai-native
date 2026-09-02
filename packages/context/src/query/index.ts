e766e0f975a4e18002c1f205dd70d97d4ddc58a2
Idebook Digital <luizanunciostoca@gmail.com>
2026-09-02 03:16:26 -0300

    feat(W06-A): reconcile query exports on current main

---
+++ packages/context/src/query/index.ts
@@ -0,0 +1,15 @@
+export { validateContextQuery } from './validate.js';
+export {
+  CONTEXT_CURRENTNESS_MODES,
+  CONTEXT_QUERY_VALIDATION_REASONS,
+  CONTEXT_SOURCE_CLASSES,
+} from './types.js';
+export type {
+  ContextCurrentnessMode,
+  ContextQuery,
+  ContextQueryLimits,
+  ContextQueryValidationReason,
+  ContextQueryValidationResult,
+  ContextSelector,
+  ContextSourceClass,
+} from './types.js';
