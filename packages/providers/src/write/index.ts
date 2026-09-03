457523f944eb170cc569d10cda12920bc0d48423
Idebook Digital <luizanunciostoca@gmail.com>
2026-09-02 22:46:58 -0300

    feat(W08-D): publish bounded write leaf exports

---
+++ packages/providers/src/write/index.ts
@@ -0,0 +1,16 @@
+export { executeGovernedProviderWrite } from './transport.js';
+export {
+  PROVIDER_WRITE_ERRORS,
+  PROVIDER_WRITE_SAFE_MODES,
+  PROVIDER_WRITE_TRANSPORT_ERRORS,
+  type ProviderWriteAdapter,
+  type ProviderWriteDependencies,
+  type ProviderWriteError,
+  type ProviderWriteRequest,
+  type ProviderWriteResult,
+  type ProviderWriteSafeMode,
+  type ProviderWriteTransportError,
+  type ProviderWriteTransportRequest,
+  type ProviderWriteTransportResult,
+  type W07ProviderExecutionProof,
+} from './types.js';
